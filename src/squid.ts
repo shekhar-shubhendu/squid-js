c690cf346542d3b8bd70c59e68d7ae8cef2a284d
Sebastian Gerske <sebastian.gerske@gmail.com>
2018-10-17 10:36:00 +0200

    return string on register method because we dont need that asset back but the asset id.

--- src/squid.ts
+++ src/squid.ts
@@ -1,13 +1,11 @@
+import Asset from "./ocean/Asset"
 import Ocean from "./ocean/Ocean"
 import Order from "./ocean/Order"
-import Account from "./ocean/Order"
-import Asset from "./ocean/Order"
 import Logger from "./utils/Logger"
 
 export {
     Ocean,
     Order,
     Asset,
-    Account,
     Logger,
 }
