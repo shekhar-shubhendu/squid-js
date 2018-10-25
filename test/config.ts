28fe11e6716d695708cf7cba3e4cf710e9fedd80
Sebastian Gerske <sebastian.gerske@gmail.com>
2018-10-25 17:18:33 +0200

    adapt test config

--- test/config.ts
+++ test/config.ts
@@ -2,4 +2,9 @@ import Config from "../src/models/Config"
 
 export default {
     nodeUri: "http://localhost:8545",
+    parityUri: "http://localhost:9545",
+    secretStoreUri: "https://secret-store.dev-ocean.com",
+    threshold: 2,
+    password: "unittest",
+    address: "0xed243adfb84a6626eba46178ccb567481c6e655d",
 } as Config
