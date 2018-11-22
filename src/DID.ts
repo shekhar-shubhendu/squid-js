ef8a8ec0f75e2b219f640c93451518ef1293c152
Bill Barman <bill@anantya.com>
2018-11-22 13:29:40 +0800

    add docs and rest of DID conversion tools

--- src/DID.ts
+++ src/DID.ts
@@ -9,6 +9,16 @@ import {URL} from "whatwg-url"
 
 const OCEAN_DID_METHOD = "op"
 
+/*
+ * This function generates all types of DID's including ocean DID's
+ * 
+ * :param didId: string to of the 'id' part of the DID
+ * :param path: option path part of the DID
+ * :param fragment: option fragment of the DID
+ * :param method: option method of the DID, defaults to 'op'
+ * 
+ * :return string generated DID, in the format did:<method>:<didId>[/<path>][#<fragment>]
+ */
 export function didGenerate(didId: string, path?: string, fragment?: string, method?: string) {
     method = method === undefined ? OCEAN_DID_METHOD : method
     method = method.toLowerCase().replace(/[^a-z0-9]/g, "")
@@ -33,6 +43,18 @@ interface IDIDParse {
     idHex?: string,
 }
 
+/*
+ * The function didParse will parse all types of DID's including Ocean DID
+ * If this is a Ocean DID the function will return record with the value
+ * `idHex` set to a hex string ( without the leading 0x ).
+ * :param did: did string to parse
+ * :return ParseRecord
+ *      method - method of DID
+ *      id - id of the DID
+ *      path - path of the DID or null
+ *      fragment - fragment of the DID or null
+ *      idHex - id as hex string without the leading '0x' or null for non Ocean DID's
+ */
 export function didParse(did: string): IDIDParse {
 
     let result: IDIDParse = null
@@ -52,13 +74,65 @@ export function didParse(did: string): IDIDParse {
             idHex: null,
         }
         if ( result.method === OCEAN_DID_METHOD && result.id.match(/^[0-9A-Fa-f]{1,64}$/) ) {
-            result.idHex = Web3.utils.toHex("0x" + result.id)
+            result.idHex = Web3.utils.toHex("0x" + result.id).substring(2)
         }
     }
     return result
 }
 
+/*
+ * Validate a Ocean DID, return true if valid, else false
+ * 
+ * :param did: string to validate as an Ocean DID
+ * :return true if the DID is valid
+ */
 export function isDIDValid(did: string): boolean {
     const result = didParse(did)
     return result && result.idHex != null
+}
+
+/*
+ * Function to convert a hex id string to an Ocean DID
+ * 
+ * :param id: can be a hex string with or without the leading '0x'
+ * :param method: if empty, default to 'op'
+ * :return a valid DID
+ * :return '0' for a 0 DID 
+ */
+export function idToDID(id: string, method?:string): string {
+    method = method === undefined ? OCEAN_DID_METHOD : method
+    method = method.toLowerCase().replace(/[^a-z0-9]/g, "")
+
+    // remove any leading 0x
+    id = id.replace(/^0x/, "")
+    
+    // return an empty DID if == 0
+    if ( id.match(/^0+$/) ) {
+        return "0"
+    }
+    return "did:" + method + ":" + Web3.utils.toHex("0x" + id).substring(2)
+}
+
+/*
+ * Function to convert an Ocean DID to an Ocean Id
+ * 
+ * :param did: string of the did to convert to an Ocean id
+ * 
+ * :return a hex string, without the leading '0x'
+ * :return null if the DID is invalid
+ */
+export function DIDToId(did: string): string {
+    const result = didParse(did)
+    if (result && result.idHex ) {
+        return result.idHex
+    }
+    return null
+}
+
+export function DIDToIdBytes(did: string): Uint8Array {
+    const result = didParse(did)
+    if (result && result.idHex) {
+        return Web3.utils.hexToBytes("0x" + result.idHex)
+    }
+    return null
 }
--- test/DID.test.ts
+++ test/DID.test.ts
@@ -19,6 +19,7 @@ describe("DID Tests", () => {
             assert(did === validDID)
 
         })
+        
         it("should parse a valid DID", async () => {
             const testId = Web3.utils.randomHex(32) + "abcdefghijklmnopqrstuvwxyz"
             const testMethod = "op"
@@ -41,7 +42,7 @@ describe("DID Tests", () => {
             assert(result.id === testId)
             assert(result.path === "/testpath")
             assert(result.fragment === "#fragment")
-            assert(result.idHex === "0x" + testId)
+            assert(result.idHex === testId)
 
         })
 
@@ -51,6 +52,33 @@ describe("DID Tests", () => {
             const validDID = "did:" + testMethod + ":" + testId
             assert(didTools.isDIDValid(validDID))
             assert(!didTools.isDIDValid(validDID + "abcdef"))
+        })
+        
+        it("should convert an Ocean Id to an Ocean DID", async () => {
+            const testId = Web3.utils.randomHex(32).substring(2)
+            const testMethod = "op"
+            const validDID = "did:" + testMethod + ":" + testId
+            assert(didTools.idToDID(testId) == validDID)
+            assert(didTools.idToDID("0x" + testId) === validDID)
+            assert(didTools.idToDID("0x00") === "0")
+            assert(didTools.idToDID("00") === "0")
+        })
+        
+        it("should convert an Ocean DID to an Ocean id", async () => {
+            const testId = Web3.utils.randomHex(32).substring(2)
+            const testMethod = "op"
+            const validDID = "did:" + testMethod + ":" + testId
+            assert(didTools.DIDToId(validDID) === testId)
+        })
+
+        it("should convert an Ocean DID to an array of bytes", async () => {
+            const testId = Web3.utils.randomHex(32).substring(2)
+            const byteId = Web3.utils.hexToBytes("0x" + testId)
+            const testMethod = "op"
+            const validDID = "did:" + testMethod + ":" + testId        
+            const bufferTest = Buffer.from(didTools.DIDToIdBytes(validDID))
+            const bufferValid = Buffer.from(byteId)
+            assert(Buffer.compare(bufferTest, bufferValid) === 0 )
         })
 
     })
