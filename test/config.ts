4e3005648369809d6a9aa6883aa4e85ea5f91010
Sebastian Gerske <sebastian.gerske@gmail.com>
2018-11-27 13:57:54 +0100

    removed config of mocked service

--- test/config.ts
+++ test/config.ts
@@ -6,8 +6,4 @@ export default {
     nodeUri: "http://localhost:8545",
     parityUri: "http://localhost:9545",
     secretStoreUri: "http://localhost:12001",
-    threshold: 0,
-    password: "unittest",
-    address: "0xed243adfb84a6626eba46178ccb567481c6e655d",
-    web3Provider: null,
 } as Config
